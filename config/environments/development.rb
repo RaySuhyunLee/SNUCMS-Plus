6ffb58a45a1f6238c6c69ef9fa48822e81fa3a31
이수현 <SuhyunLee@YasuBook.local>
2016-04-15 15:35:30 +0900

    Add test smtp account. This resolves #25

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,8 +16,19 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # Care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true 
+
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => "snu.lecture.env",
+    :password             => "djagustkd",
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
