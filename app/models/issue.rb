9da608d264833a1f94f760240dd484fd16737f9f
sanha <sanhaleehana@naver.com>
2016-04-17 06:34:08 -0700

    comment create and issue symultaneously.

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -2,4 +2,6 @@ class Issue < ActiveRecord::Base
 	validates :title, presence: :true
 	belongs_to :have_issue, :polymorphic => true
 	has_many :comments, dependent: :destroy
+
+	accepts_nested_attributes_for :comments
 end
--- app/views/comments/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @comment, :id, :contents, :commenter, :issue_id, :created_at, :updated_at
