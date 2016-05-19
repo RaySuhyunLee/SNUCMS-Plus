a6a67955b0e464472e71d0f02e2b91ebddfc7aff
sanha <sanhaleehana@naver.com>
2016-05-19 00:08:46 -0700

    Course self link

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,7 @@ class Course < ActiveRecord::Base
   has_many :issues, :as => :have_issue, dependent: :destroy 
   belongs_to :school
   belongs_to :professor
+
+  has_one :next_course, :class_name => "Course", :foreign_key => "past_course_id"
+  belongs_to :past_course, :class_name => "Course"
 end
