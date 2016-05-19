ce14dd3b561e21652d8d018bac7355d58e6e4baf
이수현 <SuhyunLee@YasuBook.local>
2016-05-19 16:37:16 +0900

    Add new migration, Modify User and Course model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160519063141) do
+ActiveRecord::Schema.define(version: 20160519070106) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "contents"
@@ -32,6 +32,14 @@ ActiveRecord::Schema.define(version: 20160519063141) do
     t.integer  "professor_id"
     t.integer  "past_course_id"
   end
+
+  create_table "courses_users", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "course_id"
+  end
+
+  add_index "courses_users", ["course_id"], name: "index_courses_users_on_course_id"
+  add_index "courses_users", ["user_id"], name: "index_courses_users_on_user_id"
 
   create_table "issues", force: :cascade do |t|
     t.string   "title"
