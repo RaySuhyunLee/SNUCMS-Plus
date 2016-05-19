3c89516a1dac6bde03505af553c70ac4db2c73a7
sanha <sanhaleehana@naver.com>
2016-05-19 00:00:57 -0700

    Model amend

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,5 @@
 class Course < ActiveRecord::Base
-  has_many :issues, :as => :have_issue, dependent: :destroy
+  has_many :issues, :as => :have_issue, dependent: :destroy 
+  belongs_to :school
+  belongs_to :professor
 end
