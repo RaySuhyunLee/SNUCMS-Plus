5447ec4212643fe6ce7e7e99201f4914c46f06e6
이수현 <SuhyunLee@YasuBook.local>
2016-05-20 13:20:30 +0900

    Add new migration. Link between issues and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable
   has_and_belongs_to_many :courses
+  has_and_belongs_to_many :issues
 end
