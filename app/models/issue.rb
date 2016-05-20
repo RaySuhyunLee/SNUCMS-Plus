bb8481a63a46b842368991f9e5a6f0d2f075f3a1
이수현 <SuhyunLee@YasuBook.local>
2016-05-20 13:20:30 +0900

    Add new migration. Link between issues and users

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -3,5 +3,7 @@ class Issue < ActiveRecord::Base
 	belongs_to :have_issue, :polymorphic => true
 	has_many :comments, dependent: :destroy
 
+  has_and_belongs_to_many :users
+
 	accepts_nested_attributes_for :comments
 end
