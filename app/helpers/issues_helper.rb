fc73f8aac4331550f0a4f9eeda528678bb7993fe
sanha <sanhaleehana@naver.com>
2016-05-26 22:40:47 -0700

    Issue link has it's title

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -1,8 +1,9 @@
 module IssuesHelper
   def render_issue_link(text)
     result = text.gsub(@regex[:issue_link]) do
-      if Issue.where("have_issue_id = ? AND have_issue_type = ? AND parent_issue_id = ?", @issue.have_issue_id, @issue.have_issue_type, $1).first != nil
-        '[issue ' + $1 + '](' + course_issue_path(@parent.id, $1) + ')'
+      @linked_issue =  Issue.where("have_issue_id = ? AND have_issue_type = ? AND parent_issue_id = ?", @issue.have_issue_id, @issue.have_issue_type, $1).first
+      if @linked_issue != nil
+        '[#' + $1 + ' ' + @linked_issue.title + '](' + course_issue_path(@parent.id, $1) + ')'
       else 
         '#' + $1
       end
