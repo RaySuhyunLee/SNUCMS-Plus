475c741bdd36634edb26fefaccda08ea25dcd92d
이수현 <SuhyunLee@YasuBook.local>
2016-06-03 21:40:30 +0900

    Implement simple dynamic loader

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,16 @@ class HomeController < ApplicationController
   def index
     @resource
   end
+
+  def load_timeline
+    offset = params['count']
+    puts "offset = #{offset}"
+    user = current_user
+    limit = 5
+    @issues = user.issues.offset(offset).take(limit)
+
+    respond_to do |format| 
+      format.all { render json: {issues: @issues} }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,9 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'home#index'
+  
+  # Home routing
+  get 'home/load_timeline'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
