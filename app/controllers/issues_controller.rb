66a4bfbe6ab66000e9c6b7206540e8b06e438741
khm <kalaluthien@naver.com>
2016-06-10 14:35:28 +0900

    fix issue regex

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,16 +1,15 @@
 class IssuesController < ApplicationController
   before_action :set_parent
   before_action :set_issue, only: [:show, :update, :destroy, :subscribe]
+  before_action :set_regex, only: [:show]
   before_action :set_comments, only: [:show, :update]
 
   # GET /(parent_type)/:(parent_id)/issues
-  # GET /(parent_type)/:(parent_id)/issues.json
   def index
     @issues = @parent.issues.all
   end
 
   # GET /(parent_type)/:(parent_id)/issues/:id
-  # GET /(parent_type)/:(parent_id)/issues/:id.json
   def show
     unless @issue.nil?
       @comments = @issue.comments.all
@@ -20,18 +19,12 @@ class IssuesController < ApplicationController
     else
       @subscribe_button_text = "좋아요"
     end
-    @regex = 
-    {
-      issue_link: /#(\d+)/,
-      link: /\[\[(.*)\]\]/,
-      latex: /(?<!\\)\$(.*)\$/
-    }
   end
 
   # GET /(parent_type)/:(parent_id)/issues/new
   def new
-  	@issue = @parent.issues.new
-	@comment = @issue.comments.new
+    @issue = @parent.issues.new
+    @comment = @issue.comments.new
   end
 
   # Freezed
@@ -40,14 +33,13 @@ class IssuesController < ApplicationController
   #end
 
   # POST /(parent_type)/:(parent_id)/issues/
-  # POST /(parent_type)/:(parent_id)/issues.json
   def create
     @issue = @parent.issues.new(issue_params)
     @parent.update_attribute(:issue_num, @parent.issue_num + 1)
     @issue.parent_issue_id = @parent.issue_num
 
     respond_to do |format|
-      if @issue.save 
+      if @issue.save
         format.html { redirect_to @index_path, notice: 'Issue was successfully created.' }
         format.json { render :show, status: :created, location: @issue }
       else
@@ -58,7 +50,6 @@ class IssuesController < ApplicationController
   end
 
   # PATCH/PUT /(parent_type)/:(parent_id)/issues/:id
-  # PATCH/PUT /(parent_type)/:(parent_id)/issues/:id.json
   def update
     respond_to do |format|
       if @issue.update(issue_params)
@@ -72,7 +63,6 @@ class IssuesController < ApplicationController
   end
 
   # DELETE /(parent_type)/:(parent_id)/issues/:id
-  # DELETE /(parent_type)/:(parent_id)/issues/:id.json
   def destroy
     @issue.destroy
     respond_to do |format|
@@ -103,18 +93,18 @@ class IssuesController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_parent
     parent_type = request.path.split('/')[1]
-	if parent_type == "courses"
-	  @parent = Course.find(params[:course_id])
+    if parent_type == "courses"
+      @parent = Course.find(params[:course_id])
       @parent_name = "Course"
       @index_path = course_issues_path(params[:course_id])
       @new_path = new_course_issue_path(params[:course_id])
-	end
+    end
   end
 
   # Find issue with url parameters.
   # The parameter :id not means issue_id, but parent_issue_id.
   def set_issue
-    # freezed @edit_path = edit_course_issue_path(params[:course_id], params[:id])	
+    # freezed @edit_path = edit_course_issue_path(params[:course_id], params[:id])
     if @parent_name == "Course"
       @issue = Issue.where("have_issue_id = ? AND have_issue_type = ? AND parent_issue_id = ?", params[:course_id], "Course", params[:id]).first
       @issue_path = course_issue_path(@issue.have_issue_id, @issue.parent_issue_id)
@@ -127,7 +117,7 @@ class IssuesController < ApplicationController
       @comments_path = course_issue_comments_path(params[:course_id], params[:id])
     end
   end
-  
+
   # Never trust parameters from the scary internet, only allow the white list through.
   def issue_params
     params.require(:issue).permit!
@@ -138,4 +128,14 @@ class IssuesController < ApplicationController
     user = current_user
     return user.issues.exists? @issue.id
   end
+
+  def set_regex
+    @regex =
+    {
+      link: /\[\[([^\]]*)\]\]/,
+      redirect: /^redirect \[\[(.*)\]\]/,
+      latex: /(?<!\\)\$(.*)\$/
+    }
+  end
+
 end
