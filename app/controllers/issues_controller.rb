605dc07760d60d87f7bf1186ce306a1e43843c9b
sanha <sanhaleehana@naver.com>
2016-06-15 22:30:35 -0700

    Footer header delete, issue create link

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -61,7 +61,7 @@ class IssuesController < ApplicationController
 
     respond_to do |format|
       if @issue.save
-        format.html { redirect_to @index_path }
+        format.html { redirect_to @index_path + "/" + @issue.parent_issue_id.to_s}
         format.json { render :show, status: :created, location: @issue }
       else
         format.html { render :new }
